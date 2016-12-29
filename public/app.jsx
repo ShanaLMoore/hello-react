9cd2f9d2b4020953c2ed1e7a9aa1a5ac690131ac
Shana L Moore <shana.lavina.moore@gmail.com>
2016-12-29 10:28:43 -0800

    nesting components in react

--- public/app.jsx
+++ public/app.jsx
@@ -1,10 +1,4 @@
-var Greeter = React.createClass({
-  getDefaultProps: function(){
-    return  {
-      name: 'React',
-      message: 'This is from the component!'
-    };
-  },
+var GreeterMessage = React.createClass({
   render: function(){
     var name = this.props.name;
     var message = this.props.message;
@@ -18,9 +12,63 @@ var Greeter = React.createClass({
   }
 });
 
-var firstName = "Andrew";
+var GreeterForm = React.createClass({
+  onFormSubmit: function(e){
+    e.preventDefault();
+
+    var name = this.refs.name.value;
+
+    if (name.length > 0){
+      this.refs.name.value = '';
+      this.props.onNewName(name);
+    }
+  },
+
+  render: function(){
+    return (
+      <div>
+        <form onSubmit={this.onFormSubmit}>
+          <input type="text" ref="name"/>
+          <button>Set Name</button>
+        </form>
+      </div>
+    );
+  }
+});
+
+var Greeter = React.createClass({
+  getDefaultProps: function(){
+    return  {
+      name: 'React',
+      message: 'This is the default message!'
+    };
+  },
+  getInitialState: function(){
+    return {
+      name: this.props.name
+    };
+  },
+  handleNewName: function(name){
+    this.setState({
+      name: name
+    });
+  },
+  render: function(){
+    var name = this.state.name;
+    var message = this.props.message;
+
+    return (
+      <div>
+        <GreeterMessage name={name} message={message} />
+        <GreeterForm onNewName={this.handleNewName}/>
+      </div>
+    );
+  }
+});
+
+var firstName = "Shana";
 
 ReactDOM.render(
-  <Greeter name={firstName} message="Message from prop"/>,
+  <Greeter name={firstName}/>,
   document.getElementById('app')
 );
